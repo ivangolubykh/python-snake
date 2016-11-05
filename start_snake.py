6fea892a1eb450ce40a98d3cc09d26273810b1b5
ivangolubykh <ivan9@allworld.xyz>
2016-11-05 18:58:14 +0300

    Изменил инструмент разработки с turtle на tkinter, нарисовал окно, холст на окне и первую функцию для прорисовки квадратов желаемого размера в нужном месте.

--- start_snake.py
+++ start_snake.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # coding: utf-8
 
-import turtle
+from tkinter import *
 import time
 import random
 
@@ -44,19 +44,36 @@ CLASS:
  Погрешность в пределах статистики, но вроде глобальная переменная чуть
 быстрее.
 
- 
+
 '''
 
 
+def element_square(x, y, d, canv):
+    # координата центра квадрата и его сторона
+    if (d % 2) == 0:
+        d +=1 # сторону квадрата делаю нечётной
+    x = x - (d // 2) # координата левой грани квадрата
+    y = y - (d // 2) # координата верхней грани квадрата
+    rect = canv.create_rectangle(x, y, x+d, y+d, fill="red", width=2)
+    return rect
+
+
 def main():
-    turtle.title('Программа Змейка на питоне в графике')
+    root = Tk()
+    root.title('Программа Змейка на питоне в графике')
+    root.geometry('800x600+150+150')
+
+    canv = Canvas(width=740,height=470,bg='#bfcff1')
+    canv.place(x=30, y=100)
+
+
 
     # turtle.onkey(pres_key_w,"w")
     # turtle.listen()
 
+    element_square(10, 50, 15, canv)
 
-
-    turtle.mainloop()
+    root.mainloop()
 
 
 
