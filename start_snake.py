94d7b4a12a2f79b20ef3c5e3379d4c39a34a896a
ivangolubykh <ivan9@allworld.xyz>
2016-11-06 19:42:48 +0300

    Была продумана идеология структуры программы почти до конца всей реализации. Существующий код почти полностью переписан. 1) функция рисования квадрата возвращает номер (id) нарисованного элемента. 2) Создал 2 новых класса для хранения глобальных констант и переменных. Предполагаю, что будет оптимальнее брать их из этих классов, чем таскать с собой при каждом обращении к новому классу или подклассу. 3) Класс snake_body теперь создаёт змею в стартовой точке, а так же перемещает её в текущем направлении на 1 шаг, с возможностью удлинения змеи (не укорачивать хвост после смещения головы) 4) При движении змеи для её отрисовки используется только отдельный класс element_square в соответствии с идеологией ООП.
    На данный момент завершены классы отрисовки фрагмента тела (и головы) змеи, а так же класс для движения змеи в текущем направлении, хранящемся в новом классе с глобальными переменными.

--- start_snake.py
+++ start_snake.py
@@ -2,121 +2,99 @@
 # coding: utf-8
 
 from tkinter import *
+from enum import Enum
 import time
 import random
 
 
-'''
- Перед началом создания программы провёл сравнения скорости
-работы при использовании глобальных переменных и собственного 
-класса
-с набором своих переменных.
- Провожу тест для изменения значения глобальной переменной и переменной
- из своего класса 100000000 раз (100М раз).
-Результат:
- global -> от 19.62 до 20.70 сек
- class  -> от 19.81 до 21.02 сек
-
-GLOBAL:
-    start = time.time()
-    def sum():
-        global k
-        k += 1
-    k = 0
-    i = 0
-    while (i < 100000000):
-        sum()
-        i += 1
-    print(str(k) + ' - ' + str((time.time() - start)) )
-
-CLASS:
-    start = time.time()
-    class glob():
-        k=0;
-    test = glob()
-    i = 0
-    while (i < 100000000):
-        test.k += 1
-        i += 1
-    print(str(test.k) + ' - ' + str((time.time() - start)) )
-
-ВЫВОД:
- Погрешность в пределах статистики, но вроде глобальная переменная чуть
-быстрее.
+class CONST(Enum): # Список возможных направлений движения и других констант
+    RIGHT = 1
+    DOWN = 2
+    LEFT = 3
+    UP = 4
+    SNAKE_X = 370 # Координата старта змеи
+    SNAKE_Y = 235 # Координата старта змеи
+    SNAKE_HCOLOR = 'red' # Цвет головы змейки
+    SNAKE_BCOLOR = 'green' # Цвет тела змейки
+    CANVAS_BGCOLOR = '#bfcff1' # Цвет фона холста
+    SNAKE_THICKNESS = 10 # Толщина тела змейки
 
 
-'''
+class vals: # список изменяемых глобальных объектов/переменных
+    vector = CONST.RIGHT.value # Текущее направление движения
+    canv = 0
 
 
-class element_square:
-    def __init__(self, x, y, d, color, canv):
+class element_square: # Рисую квадратик со стороной d и центром x,y
+    def __init__(self, x, y, d, color):
         self.x = x
         self.y = y
         self.d = d
         self.color = color
-        self.canv = canv
         if (self.d % 2) == 0:
             self.d +=1 # сторону квадрата делаю нечётной
 
     def draw(self):
         x = self.x - (self.d // 2) # координата левой грани квадрата
         y = self.y - (self.d // 2) # координата верхней грани квадрата
-        self.object = self.canv.create_rectangle(x, y, x + self.d, y + self.d, fill=self.color, width=2)
+        return vals.canv.create_rectangle(x, y, x + self.d, y + self.d, fill=self.color, width=2)
 
 
-class snake_body:
-    def __init__(self, x, y, bloks, thickness, color, canv):
-        self.x = x
-        self.y = y
-        self.bloks = bloks
-        self.thickness = thickness # толщина блока тела
-        self.color = color
-        self.canv = canv
+class snake_body: # Двигать тело змеюки в текущую сторону на 1 шаг
+# При этом тело может увеличиться (add='add') в размерах или нет
+    def __init__(self):
+        self.head = element_square(CONST.SNAKE_X.value, CONST.SNAKE_Y.value,
+                             CONST.SNAKE_THICKNESS.value,
+                             CONST.SNAKE_HCOLOR.value)
         self.body = []
-        
-    def draw(self):
-        for i in range(0,len(self.body)):
-            x = self.body[i].x - (self.thickness // 2) # координата левой грани квадрата
-            y = self.body[i].y - (self.thickness // 2) # координата верхней грани квадрата
-            self.body[i] = self.canv.create_rectangle(x, y, x + self.thickness, y + self.thickness, fill=self.color, width=2)
+        self.body.append(self.head.draw())
 
+    def step(self, add):
+        if vals.vector == CONST.RIGHT.value:
+            deltax = CONST.SNAKE_THICKNESS.value
+            deltay = 0
+        elif vals.vector == CONST.DOWN.value:
+            deltax = 0
+            deltay = CONST.SNAKE_THICKNESS.value
+        elif vals.vector == CONST.LEFT.value:
+            deltax = CONST.SNAKE_THICKNESS.value
+            deltay = 0
+        elif vals.vector == CONST.UP.value:
+            deltax = 0
+            deltay = -CONST.SNAKE_THICKNESS.value
+        self.head.x += deltax
+        self.head.y += deltay
+        self.head = element_square(self.head.x, self.head.y,
+                             CONST.SNAKE_THICKNESS.value,
+                             CONST.SNAKE_HCOLOR.value)
+        self.body.append(self.head.draw()) # Создал новую голову
+        vals.canv.itemconfig(self.body[-2],
+                             fill=CONST.SNAKE_BCOLOR.value) # Перекрасил старую голову в тело
+        if add != 'add':
+            vals.canv.delete(self.body[0])
+            self.body.pop(0)
 
-class horiz_body(snake_body):
-    def __init__(self, x, y, bloks, thickness, color, canv):
-        super().__init__(x, y, bloks, thickness, color, canv)
-        for i in range(0,self.bloks):
-            self.body.append(element_square(self.x + i * self.thickness, self.y, self.thickness, self.color, self.canv))
-
-
-class vert_body(snake_body):
-    def __init__(self, x, y, bloks, thickness, color, canv):
-        super().__init__(x, y, bloks, thickness, color, canv)
-        for i in range(0,self.bloks):
-            self.body.append(element_square(self.x, self.y + i * self.thickness, self.thickness, self.color, self.canv))
 
 
 
 
 
 def main():
-    canvasbgcolor = '#bfcff1' # Цвет фона холста
-    snake_head_color = 'red' # Цвет головы змейки
-    snake_body_color = 'green' # Цвет тела змейки
-    snake_thickness = 10 # Толщина тела змейки
     root = Tk()
     root.title('Программа Змейка на питоне в графике')
     root.geometry('800x600+150+150')
 
-    canv = Canvas(width=740,height=470,bg=canvasbgcolor)
-    canv.place(x=30, y=100)
+    vals.canv = Canvas(width=740,height=470,bg=CONST.CANVAS_BGCOLOR.value)
+    vals.canv.place(x=30, y=100)
 
 
-
-    hbody1 = horiz_body(30, 200, 15, snake_thickness, snake_body_color, canv)
-    hbody1.draw()
-
-    vbody1 = vert_body(330, 200, 15, snake_thickness, snake_body_color, canv)
-    vbody1.draw()
+    snake = snake_body()
+    snake.step('add')
+    snake.step('add')
+    snake.step('del')
+    snake.step('del')
+    snake.step('add')
+    snake.step('add')
 
 
 
